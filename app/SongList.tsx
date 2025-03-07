9996c0d85668a74e55fb8b53397d290b70d8108a
ms358 <ms358@njit.edu>
2025-03-06 22:20:56 -0500

    added songlistpage

---
+++ app/SongList.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function SongList() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Song List</ThemedText>
+      <ThemedText>Coming soon!</ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
